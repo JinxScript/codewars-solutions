7305c5bdf5221d2293c1e3c5dc5b56418832df06
Omolemo Tshwaolesele <omolemotswaolesele@gmail.com>
2022-06-20 19:07:16 +0200

    personalized message added which uses .equals method

---
+++ 8kyu/personalMessage.java
@@ -0,0 +1,28 @@
+/*
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+FUNDAMENTALS
+
+*/
+
+
+//My soulution 
+//USING AN EQUAL DOESNT WORK
+class Kata {
+    static String greet(String name, String owner) {
+     if (name.equals(owner)){
+       return "Hello boss";
+     }else {
+           return "Hello guest"; 
+     }
+     
+   }   
+}
+
+
