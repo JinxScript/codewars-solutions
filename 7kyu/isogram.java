eecf7d6605c43f8bd8cc023b6322cf8575662b88
Omolemo Tshwaolesele <omolemotswaolesele@gmail.com>
2022-06-28 18:59:19 +0200

    added altenative solution

--- 7kyu/isogram.java
+++ 7kyu/isogram.java
@@ -33,3 +33,25 @@ public class isogram {
      }return true ;
     } 
 }
+
+
+//best solutions to
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class isogram {
+  public static boolean isIsogram(String str) {
+    Set<Character> letters = new HashSet<Character>();
+         
+    for (int i = 0; i < str.length(); ++i) {
+      if (letters.contains(str.toLowerCase().charAt(i))) {
+        return false;        
+      }
+      
+      letters.add(str.charAt(i));
+    }
+         
+    return true;
+  }
+}
