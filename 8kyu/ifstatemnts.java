5184f3ad8ca85b7df0ba58b03815a7b6cf9c4887
Omolemo Tshwaolesele <omolemotswaolesele@gmail.com>
2022-07-22 14:02:29 +0200

    if statements

---
+++ 8kyu/ifstatemnts.java
@@ -0,0 +1,41 @@
+/*Grade book
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+FUNDAMENTALS*/
+
+public class GrassHopper {
+
+    public static char getGrade(int s1, int s2, int s3) {
+      int ave = (s1 + s2 + s3 )/3;
+      
+            if (ave >= 90 && ave <= 100) {
+            return 'A';
+        } else if (ave >= 80 && ave < 90) {
+          return 'B';
+        } else if (ave >= 70&& ave < 80) {
+          return 'C';
+        } else if (ave >= 60  && ave < 70) {
+          return 'D';
+        } else {
+          return 'F';
+    }    }
+}
+
+
+
+//other solutions
+public class GrassHopper {
+
+    public static char getGrade(int s1, int s2, int s3) {
+        s1=(s1+s2+s3)/3;
+        return s1 >= 90 ? 'A':s1 >= 80 ? 'B':s1 >= 70 ? 'C':s1 >= 60 ? 'D':'F';
+    }
+}
